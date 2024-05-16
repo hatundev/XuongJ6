9e9e516a2c343ae6779a590b3c5b4cb5150d87c2
HatunDev <hatun35.dev@gmail.com>
2024-05-16 15:24:11 +0700

    add new product

---
+++ src/main/java/com/poly/XuongJ6/model/request/NewProduct.java
@@ -0,0 +1,12 @@
+package com.poly.XuongJ6.model.request;
+
+public class NewProduct {
+    private String productName;
+    private String color;
+    private Integer quantity;
+    private Float sellPrice;
+    private Float originPrice;
+    private Integer brandId;
+    private Integer subCateId;
+    private Integer statusId;
+}
